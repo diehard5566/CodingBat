a79d53802814239e0483e13dc5b30ca80d0a7626
diehard5566 <shihyao001@gmail.com>
2022-02-09 21:29:47 +0800

    xyz+註解

--- String2/xyz_there.py
+++ String2/xyz_there.py
@@ -1,5 +1,9 @@
 # Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.).
 #  So "xxyz" counts but "x.xyz" does not.
+#      0123456789 
+#str = abc.xyzxyz
+#      xixx
+
 
 def xyz_there(str):
     if len(str) >= 3 and str[0:3] == "xyz":
